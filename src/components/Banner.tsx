d080a854a39834b9028949c0f5f7e7d37c54358b
knwpound <pound.somparn@gmail.com>
2025-10-08 17:41:58 +0700

    Create Banner.tsx

---
+++ src/components/Banner.tsx
@@ -0,0 +1,11 @@
+export function Banner() {
+  return (
+    <div
+      className="w-full h-[400px] bg-[url(/pics/banner1.png)] bg-cover bg-[center_40%] py-15
+        flex flex-col justify-center items-center"
+    >
+      <h1 className="text-5xl font-caveat">Enjoy every moment, </h1>
+      <h1 className="text-5xl font-caveat">create memories that last. </h1>
+    </div>
+  );
+}
