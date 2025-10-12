85a7d8b172012e68d3ed0ca242fc75ec73820077
knwpound <pound.somparn@gmail.com>
2025-10-12 15:50:14 +0700

    create: tid page

---
+++ src/app/(ticketinfo)/ticket/[tid]/page.tsx
@@ -0,0 +1,43 @@
+import { DefaultButton } from "@/components/reused/DefaultButton";
+import { LightButton } from "@/components/reused/LightButton";
+import Image from "next/image";
+export default function TicketDetail() {
+  return (
+    <div className="w-full flex flex-row pt-25 px-5 justify-center gap-5">
+      <div className="relative w-[50%] h-[300px] shadow-md">
+        <Image
+          src={"/pics/banner2.png"}
+          alt=""
+          fill
+          className="object-cover rounded-lg"
+        ></Image>
+      </div>
+      <div className="bg-white flex flex-col justify-center px-5 rounded-md gap-5 py-2 shadow-md">
+        <div>
+            <h1 className="text-2xl font-semibold">The Phomtom of Opera</h1>
+        <p>Ticket id #abcdef</p>
+        </div>
+        <div>
+          <div className="flex flex-col gap-0">
+            <p className="text-sm text-gray-500 font-semibold">Name</p>
+            <p className="text-lg text-black font-semibold">Kanokwan</p>
+            <div className="flex flex-col gap-0">
+              <p className="text-sm text-gray-500 font-semibold">Date</p>
+              <p className="text-lg text-black font-semibold">21 Oct</p>
+            </div>
+            <div className="flex flex-col gap-0">
+              <p className="text-sm text-gray-500 font-semibold">
+                Total Tickets
+              </p>
+              <p className="text-lg text-black font-semibold">4</p>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex flex-row items-end gap-3 justify-end">
+            <LightButton text="Cancel"/>
+            <DefaultButton text="Edit"/>
+        </div>
+      </div>
+    </div>
+  );
+}
