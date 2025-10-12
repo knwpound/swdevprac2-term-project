118a5d7a498cdc5f01966cf9b342b76644451235
knwpound <pound.somparn@gmail.com>
2025-10-12 18:20:54 +0700

    Create DefaultInput.tsx

---
+++ src/components/reused/DefaultInput.tsx
@@ -0,0 +1,11 @@
+export function DefaultInput() {
+  return (
+    <input
+      type="text"
+      placeholder="Value"
+      className="bg-gray-200 outline outline-gray-300
+                            py-1 px-3 rounded-sm focus:outline-gray-400 focus:outline-2
+                            transition duration-150"
+    />
+  );
+}
