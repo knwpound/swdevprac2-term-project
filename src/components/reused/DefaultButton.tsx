af6777a26c6ef5ba10bfae6a863a5337d8b276fb
knwpound <pound.somparn@gmail.com>
2025-10-14 23:10:40 +0700

    Update DefaultButton.tsx

--- src/components/reused/DefaultButton.tsx
+++ src/components/reused/DefaultButton.tsx
@@ -1,6 +1,17 @@
-export function DefaultButton({text}:{text:string}){
-    return(
-        <button className="font-semibold text-white bg-black px-4 py-1 rounded-md
-        hover:text-black hover:bg-white hover:outline hover:outline-black">{text}</button>
-    )
+export interface DefaultButtonProps {
+  text: string;
+  onClick?: () => void;
+}
+
+export function DefaultButton({ text, onClick }: DefaultButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className="font-semibold text-white bg-black px-4 py-1 rounded-md
+        hover:text-black hover:bg-white hover:outline hover:outline-1 hover:outline-black
+        transition duration-300 cursor-pointer"
+    >
+      {text}
+    </button>
+  );
 }
