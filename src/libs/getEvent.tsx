0e9b7aa2d77780f9c9b77bfd46586dc491776c55
knwpound <pound.somparn@gmail.com>
2025-10-15 12:48:59 +0700

    Update getEvent.tsx

--- src/libs/getEvent.tsx
+++ src/libs/getEvent.tsx
@@ -1,4 +1,4 @@
-export default async function getCar(id:string) {
+export default async function getEvent(id:string) {
     const response = await fetch(`http://localhost:5000/api/v1/events/${id}`)
     if(!response.ok){
         throw new Error(`Failed to fecth event ${id}`)
