af5ca289dcdb9a783b0debd7ea8a4ae616626359
knwpound <pound.somparn@gmail.com>
2025-10-15 12:48:49 +0700

    Update EventDetailCard.tsx

--- src/components/EventDetailCard.tsx
+++ src/components/EventDetailCard.tsx
@@ -1,25 +1,35 @@
-export function EventDetailCard(){
-    return(
-        <div className="w-[250px] flex flex-col gap-2 bg-white px-5 py-5 rounded-md shadow-md">
-          <div className="">
-            <p className="text-md font-bold">Event Date</p>
-            <p className="text-sm">2025 September 12th</p>
-            <p className="text-sm">12:00 - 14:00</p>
-          </div>
-          <div className="">
-            <p className="text-md font-bold">Venue</p>
-            <p className="text-sm">
-              Bankok International Trade & Exhibition Centre (BITEC)
-            </p>
-          </div>
-          <div className="">
-            <p className="text-md font-bold">Organizer</p>
-            <p className="text-sm">SMTrue</p>
-          </div>
-          <div className="">
-            <p className="text-md font-bold">Ticket</p>
-            <p className="text-sm">350 Available</p>
-          </div>
-        </div>
-    )
+export function EventDetailCard({
+  date,
+  time,
+  venue,
+  organizer,
+  ticket,
+}: {
+  date: string;
+  time: string;
+  venue: string;
+  organizer: string;
+  ticket: string;
+}) {
+  return (
+    <div className="w-[250px] flex flex-col gap-2 bg-white px-5 py-5 rounded-md shadow-md">
+      <div className="">
+        <p className="text-md font-bold">Event Date</p>
+        <p className="text-sm">{date}</p>
+        <p className="text-sm">{time}</p>
+      </div>
+      <div className="">
+        <p className="text-md font-bold">Venue</p>
+        <p className="text-sm">{venue}</p>
+      </div>
+      <div className="">
+        <p className="text-md font-bold">Organizer</p>
+        <p className="text-sm">{organizer}</p>
+      </div>
+      <div className="">
+        <p className="text-md font-bold">Ticket</p>
+        <p className="text-sm">{ticket} Available</p>
+      </div>
+    </div>
+  );
 }
