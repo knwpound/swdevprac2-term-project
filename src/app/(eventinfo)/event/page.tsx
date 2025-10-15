1d28e4e72c8f59462ad4d1f9a264180e1c777fea
knwpound <pound.somparn@gmail.com>
2025-10-15 18:00:42 +0700

    add : add button

--- src/app/(eventinfo)/event/page.tsx
+++ src/app/(eventinfo)/event/page.tsx
@@ -2,11 +2,16 @@ import { Banner } from "@/components/Banner";
 import { CardContainer } from "@/components/CardContainer";
 import { Pagination } from "@/components/reused/Pagination";
 import getEvents from "@/libs/getEvents";
-import { Plus } from 'lucide-react';
+import { Plus } from "lucide-react";
 import { Suspense } from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import NextAuthProvider from "../../../../providers/NextAuthProvider";
+import Link from "next/link";
 
-export default function Events() {
-  const events = getEvents()
+export default async function Events() {
+  const events = getEvents();
+  const session = await getServerSession(authOptions);
   return (
     <div className="w-full">
       <Banner />
@@ -14,23 +19,30 @@ export default function Events() {
         <div className="flex flex-row justify-between">
           <h1 className="font-serif font-bold text-2xl">Events</h1>
           <div className="flex flex-row gap-1">
-          <input
-            type="text"
-            placeholder="Search"
-            className="w-[300px] bg-gray-200 px-2 rounded-md"
-          />  
-          <Plus size={"30"} strokeWidth={"3"} className="h-full rounded-md p-1 text-gray-500 
-          hover:bg-gray-200 hover:text-gray-700 active:text-black active:bg-gray-300 transition duration-150"/>
+            <input
+              type="text"
+              placeholder="Search"
+              className="w-[300px] bg-gray-200 px-2 rounded-md"
+            />
+            {session?.user.role === "admin" ? (
+              <Link href={"/event/add"}>
+                <Plus
+                  size={30}
+                  strokeWidth={3}
+                  className="h-full rounded-md p-1 text-gray-500 
+               hover:bg-gray-200 hover:text-gray-700 
+               active:text-black active:bg-gray-300 
+               transition duration-150"
+                />
+              </Link>
+            ) : null}
           </div>
-          
         </div>
         <Suspense fallback={<p>Loading...</p>}>
-          <CardContainer eventJson={events}/>
+          <CardContainer eventJson={events} />
         </Suspense>
-        
-        <Pagination/>
+        <Pagination />
       </div>
-      
     </div>
   );
 }
