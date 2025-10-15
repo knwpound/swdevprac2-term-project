ddc9c7f73385225678a077834b8ff98a4270abc6
knwpound <pound.somparn@gmail.com>
2025-10-15 12:49:04 +0700

    Update getEvents.tsx

--- src/libs/getEvents.tsx
+++ src/libs/getEvents.tsx
@@ -1,6 +1,6 @@
 import { resolve } from "path"
 
-export default async function getCars() {
+export default async function getEvents() {
     await new Promise((resolve)=>setTimeout(resolve,5000))
 
     const response = await fetch("http://localhost:5000/api/v1/events")
