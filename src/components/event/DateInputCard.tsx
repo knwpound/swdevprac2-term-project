88cd3767e78c0e9d936061b34dfbb54de12ad355
knwpound <pound.somparn@gmail.com>
2025-10-15 18:00:14 +0700

    add: logic & mui

--- src/components/event/DateInputCard.tsx
+++ src/components/event/DateInputCard.tsx
@@ -1,35 +1,31 @@
-export function DateInputCard() {
+"use client";
+import * as React from "react";
+import { DatePicker, TimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs, { Dayjs } from "dayjs";
+
+export function DateInputCard({onChangeDate,onChangeTime}:{onChangeDate:(newValue: Dayjs | null) => void,onChangeTime:(newValue: Dayjs | null) => void}) {
+
   return (
     <div className="w-full h-full flex flex-col justify-between p-5 bg-white rounded-md shadow-md">
       <h1 className="text-lg font-semibold">Date & Time</h1>
       <div className="flex flex-row gap-2 items-center justify-center">
-        <div className="w-full flex flex-col gap-1">
-          <p className="text-sm font-semibold">Select a day</p>
-          <input
-            type="text"
-            placeholder="Title"
-            className="w-full bg-gray-200 outline outline-gray-300
-                            py-1 px-3 rounded-sm"
-          />
-        </div>
-        <div className="w-[30%] flex flex-col gap-1">
-          <p className="text-sm font-semibold">Start time</p>
-          <input
-            type="text"
-            placeholder="Title"
-            className="w-full bg-gray-200 outline outline-gray-300
-                            py-1 px-3 rounded-sm"
-          />
-        </div>
-        <div className="w-[30%] flex flex-col gap-1">
-          <p className="text-sm font-semibold">End time</p>
-          <input
-            type="text"
-            placeholder="Title"
-            className="w-full bg-gray-200 outline outline-gray-300
-                            py-1 px-3 rounded-sm"
-          />
-        </div>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <div className="w-[50%] flex flex-col gap-1">
+            <p className="text-sm font-semibold">Select a day</p>
+            <DatePicker
+              onChange={onChangeDate}
+              slotProps={{ textField: { size: "small", fullWidth: true } }}
+            />
+          </div>
+          <div className="w-[30%] flex flex-col gap-1">
+            <p className="text-sm font-semibold">Start time</p>
+            <TimePicker
+              onChange={onChangeTime}
+              slotProps={{ textField: { size: "small", fullWidth: true } }}
+            />
+          </div>
+        </LocalizationProvider>
       </div>
     </div>
   );
