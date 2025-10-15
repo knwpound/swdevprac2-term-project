ba46fb035a8a4b5108969b99d719893b859398b6
knwpound <pound.somparn@gmail.com>
2025-10-15 12:48:41 +0700

    add: events fetching card

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -1,11 +1,25 @@
-import { EventCard } from "./EventCard"
-export function CardContainer(){
-    return(
-        <div className="w-full flex flex-row gap-6 justify-center">
-            <EventCard name="CU Freshy Night 2025" time="2025" venue="SCCU"/>
-            <EventCard name="CU Freshy Night 2025" time="2025" venue="SCCU"/>
-            <EventCard name="CU Freshy Night 2025" time="2025" venue="SCCU"/>
-            <EventCard name="CU Freshy Night 2025" time="2025" venue="SCCU"/>
-          </div>
-    )
+import { EventCard } from "./EventCard";
+import Link from "next/link";
+
+export async function CardContainer({
+  eventJson,
+}: {
+  eventJson: Promise<EventJson>;
+}) {
+  const eventJsonReady = await eventJson;
+  console.log(eventJsonReady);
+  return (
+    <div className="w-full flex flex-row flex-wrap gap-6 justify-center">
+      {eventJsonReady.data.map((eventItem: EventItem) => (
+        <Link href={`/event/${eventItem._id}`}>
+          <EventCard
+            key = {eventItem._id}
+            name={eventItem.name}
+            time={eventItem.eventDate}
+            venue={eventItem.venue}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 }
