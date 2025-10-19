ffce5f50ec6ddc7089acc60407306cc7fb4a09a7
knwpound <pound.somparn@gmail.com>
2025-10-19 22:54:37 +0700

    add: disabled button

--- src/components/reused/Button.tsx
+++ src/components/reused/Button.tsx
@@ -1,27 +1,31 @@
 export interface DefaultButtonProps {
   text: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-export function DefaultButton({ text, onClick }: DefaultButtonProps) {
+export function DefaultButton({ text, onClick, disabled }: DefaultButtonProps) {
   return (
     <button
       onClick={onClick}
       className="font-semibold text-white bg-black px-4 py-1 rounded-md
-        hover:text-black hover:bg-white hover:outline hover:outline-1 hover:outline-black
-        transition duration-300 cursor-pointer"
+    hover:text-black hover:bg-white hover:outline hover:outline-1 hover:outline-black
+    disabled:hover:text-white disabled:hover:bg-black disabled:hover:outline-none
+    transition duration-300 cursor-pointer disabled:opacity-30"
+      disabled={disabled}
     >
       {text}
     </button>
   );
 }
 
-export function SecondaryButton({ text, onClick }: DefaultButtonProps) {
+export function SecondaryButton({ text, onClick, disabled }: DefaultButtonProps) {
   return (
     <button
       className="font-semibold px-4 py-1 rounded-md bg-slate-100
-        hover:bg-slate-300 transition duration-300 cursor-pointer"
+        hover:bg-slate-300 transition duration-300 cursor-pointer disabled:opacity-75"
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
