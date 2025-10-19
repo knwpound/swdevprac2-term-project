af16b6ccdbdfc1fce7f1cb25aea8bc801a07d1a5
knwpound <pound.somparn@gmail.com>
2025-10-19 21:57:02 +0700

    add: values

--- src/components/event/DateInputCard.tsx
+++ src/components/event/DateInputCard.tsx
@@ -1,11 +1,24 @@
 "use client";
 import * as React from "react";
-import { DatePicker, TimePicker, LocalizationProvider } from "@mui/x-date-pickers";
+import {
+  DatePicker,
+  TimePicker,
+  LocalizationProvider,
+} from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import dayjs, { Dayjs } from "dayjs";
 
-export function DateInputCard({onChangeDate,onChangeTime}:{onChangeDate:(newValue: Dayjs | null) => void,onChangeTime:(newValue: Dayjs | null) => void}) {
-
+export function DateInputCard({
+  onChangeDate,
+  onChangeTime,
+  dateValue,
+  timeValue,
+}: {
+  onChangeDate: (newValue: Dayjs | null) => void;
+  onChangeTime: (newValue: Dayjs | null) => void;
+  dateValue?: Dayjs | null;
+  timeValue?: Dayjs | null;
+}) {
   return (
     <div className="w-full h-full flex flex-col justify-between p-5 bg-white rounded-md shadow-md">
       <h1 className="text-lg font-semibold">Date & Time</h1>
@@ -16,6 +29,7 @@ export function DateInputCard({onChangeDate,onChangeTime}:{onChangeDate:(newValu
             <DatePicker
               onChange={onChangeDate}
               slotProps={{ textField: { size: "small", fullWidth: true } }}
+              value={dateValue}
             />
           </div>
           <div className="w-[30%] flex flex-col gap-1">
@@ -23,6 +37,7 @@ export function DateInputCard({onChangeDate,onChangeTime}:{onChangeDate:(newValu
             <TimePicker
               onChange={onChangeTime}
               slotProps={{ textField: { size: "small", fullWidth: true } }}
+              value={timeValue}
             />
           </div>
         </LocalizationProvider>
