230f11eb2476edf632b621ac22602236dff81818
knwpound <pound.somparn@gmail.com>
2025-10-20 22:27:37 +0700

    Update interface.ts

--- interface.ts
+++ interface.ts
@@ -1,17 +1,39 @@
 interface EventItem {
-    _id: string,
-    name: string,
-    description: string,
-    eventDate: string,
-    venue: string,
-    organizer: string,
-    availableTicket: number,
-    posterPicture: string,
-    __v: number,
-  }
-  
-  interface EventJson {
-    success: boolean,
-    count: number,
-    data: EventItem[]
-  }
+  _id: string;
+  name: string;
+  description: string;
+  eventDate: string;
+  venue: string;
+  organizer: string;
+  availableTicket: number;
+  posterPicture: string;
+  __v: number;
+}
+
+interface EventJson {
+  success: boolean;
+  count: number;
+  data: EventItem[];
+}
+
+interface Event {
+  _id: string;
+  name: string;
+  description: string;
+  eventDate: string;
+  venue: string;
+  availableTicket: number;
+}
+
+interface TicketItem {
+  _id: string;
+  user: string; 
+  event: Event;
+  ticketAmount: number;
+}
+
+interface TicketJson {
+  success: boolean;
+  count: number;
+  data: TicketItem[];
+}
