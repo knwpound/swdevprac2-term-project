c350169fc91b50328e5c26ef298d18f12dad1a00
knwpound <pound.somparn@gmail.com>
2025-10-20 22:25:42 +0700

    Create getTicket.tsx

---
+++ src/libs/getTicket.tsx
@@ -0,0 +1,13 @@
+export default async function getTicket(id:string,token:string) {
+    const response = await fetch(`http://localhost:5000/api/v1/ticketing/${id}`,{
+        method: "GET",
+        headers:{
+            authorization: `Bearer ${token}`,
+        },
+    })
+    if(!response.ok){
+        throw new Error(`Failed to fecth ticket ${id}`)
+    }
+
+    return await response.json()
+}
