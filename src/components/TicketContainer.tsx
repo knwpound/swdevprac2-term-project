b6efd5aa899c632d61c359e078fcc40820b30b02
knwpound <pound.somparn@gmail.com>
2025-10-20 22:26:01 +0700

    add: loops in ticket

--- src/components/TicketContainer.tsx
+++ src/components/TicketContainer.tsx
@@ -1,10 +1,26 @@
-import { TicketCard } from "./TicketCard"
-export function TicketContainer(){
-    return(
-        <div className="w-full flex flex-row gap-6 justify-center">
-            <TicketCard name="CU Freshy Night 2025" time="20:00" date="21 Sep" amount={"2"}/>
-            <TicketCard name="CU Freshy Night 2025" time="20:00" date="21 Sep" amount={"2"}/>
-            <TicketCard name="CU Freshy Night 2025" time="20:00" date="21 Sep" amount={"2"}/>
-          </div>
-    )
+import { TicketCard } from "./TicketCard";
+import Link from "next/link";
+import { Suspense } from "react";
+
+export async function TicketContainer({
+  ticketJson,
+}: {
+  ticketJson: Promise<TicketJson>;
+}) {
+  const ticketJsonReady = await ticketJson;
+  console.log(ticketJsonReady)
+  return (
+    <div className="w-full flex flex-row flex-wrap gap-6 justify-center">
+      {ticketJsonReady.data.map((ticketItem: TicketItem) => (
+        <Link key={ticketItem._id} href={`/ticket/${ticketItem._id}`}>
+          <TicketCard
+            key={ticketItem._id}
+            name={ticketItem.event.name}
+            date={ticketItem.event.eventDate}
+            amount={ticketItem.ticketAmount}
+          />
+        </Link>
+      ))}
+    </div>
+  );
 }
