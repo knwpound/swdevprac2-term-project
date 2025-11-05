79c115e50918f666dd0137566a7762041b7dc4f7
knwpound <pound.somparn@gmail.com>
2025-11-05 14:54:09 +0700

    fix: not redirect to react-auth error page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,23 +1,42 @@
-'use client'
+"use client";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-    await signIn("credentials", {
+    setLoading(true);
+    setError("");
+
+    const response = await signIn("credentials", {
       email,
       password,
-      callbackUrl: "/", 
+      callbackUrl: "/",
+      redirect: false,
     });
+
+    if (response?.error) {
+      setError(response.error);
+    } else if (response?.ok) {
+      window.location.href = response.url || "/";
+    }
+
+    setLoading(false);
   };
+ 
   return (
     <div className="w-full h-screen bg-[url(/pics/cheers.jpg)] bg-cover">
       <main className="w-full h-full flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-center items-center w-fit h-fit bg-white/80 px-10 py-15 rounded-lg gap-7 shadow-md
-        max-sm:px-5 max-sm:py-8">
+        <div
+          className="flex flex-col justify-center items-center w-fit h-fit bg-white/80 px-10 py-15 rounded-lg gap-7 shadow-md
+        max-sm:px-5 max-sm:py-8"
+        >
           <div className="flex flex-col gap-2 items-center">
             <h1 className="font-serif p-0 m-0 text-3xl text-center font-bold max-sm:text-xl">
               Sign in with Email
@@ -30,35 +49,37 @@ export default function Login() {
           <div className="flex flex-col gap-4 w-[80%]">
             <div className="flex flex-col gap-2 w-full">
               <input
-              type="text"
-              className="w-full text-black text-sm px-3 py-1.5 placeholder:black border border-1 border-black 
+                type="text"
+                className="w-full text-black text-sm px-3 py-1.5 placeholder:black border border-1 border-black 
             rounded-sm focus:outline focus:outline-gray-500"
-              placeholder="Enter your Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              className="w-full text-black text-sm px-3 py-1.5 placeholder:black border border-1 border-black 
+                placeholder="Enter your Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                className="w-full text-black text-sm px-3 py-1.5 placeholder:black border border-1 border-black 
             rounded-sm focus:outline focus:outline-gray-500"
-              placeholder="Enter your Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />  
+                placeholder="Enter your Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-            
+            {error && <p className="text-red-500 text-sm">{error}</p>}
             <button
               className="w-full bg-black text-white font-medium px-3 py-1.5 rounded-md border border-1
             hover:bg-white hover:text-black hover:border hover:border-1 hover:border-black transition duration-200
             cursor-pointer"
-            onClick={handleLogin}
+              onClick={handleLogin}
             >
-              Sign in
+              {loading ? "Login..." : "Login"}
             </button>
           </div>
 
           <div className="flex flex-row gap-2">
             <p className="text-sm">Don't have an account?</p>
-            <a className="text-sm font-semibold hover:underline active:font-bold cursor-pointer transition duration-150"
-            href="/auth/register">
+            <a
+              className="text-sm font-semibold hover:underline active:font-bold cursor-pointer transition duration-150"
+              href="/auth/register"
+            >
               Register
             </a>
           </div>
