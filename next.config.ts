646106750505bf4ab32f6da9aae22d0737485cdc
knwpound <pound.somparn@gmail.com>
2025-11-06 20:15:42 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,34 @@
+// next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['drive.google.com']
-  },
-  async headers() {
-      return [
-          {
-              // matching all API routes
-              source: "/venue/:path*",
-              headers: [
-                  { key: "Access-Control-Allow-Credentials", value: "true" },
-                  { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                  { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-              ]
-          }
-      ]
-  }
+    images: {
+        domains: ['drive.google.com']
+    },
+    async headers() {
+        return [
+
+            {
+                source: "/event/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // อนุญาตให้โดเมนใดๆ เรียกใช้งานได้
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            },
+            
+            {
+                source: "/ticket/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // อนุญาตให้โดเมนใดๆ เรียกใช้งานได้
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ];
+    }
 };
 
 export default nextConfig;
