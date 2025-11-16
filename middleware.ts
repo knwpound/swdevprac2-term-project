c47c7e744470da32287a358a09a42f5ce7d4481c
knwpound <pound.somparn@gmail.com>
2025-11-16 18:25:17 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from 'next-auth/middleware'
 
 export const config = {
-    matcher: ["/ticket/:path*","/event/add","/event/:eid/edit"]
+    //matcher: ["/ticket/:path*","/event/add","/event/:eid/edit"]
 }
