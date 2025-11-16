caa4968efedca6d97acc419accce3a00ad1c8bd2
knwpound <pound.somparn@gmail.com>
2025-11-16 20:12:47 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
-export { default } from 'next-auth/middleware'
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth({
+  pages: { signIn: "/api/auth/signin" }, 
+});
 
 export const config = {
-    //matcher: ["/ticket/:path*","/event/add","/event/:eid/edit"]
-}
+  matcher: ["/ticket/:path*", "/event/add", "/event/:eid/edit"], // protected routes
+};
